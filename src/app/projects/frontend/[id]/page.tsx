11ea081b2f9dd1b22d22222ee73de736afffa41a
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2024-08-21 20:58:51 -0300

    feat: implement basic integration for dynamic routing

--- src/app/projects/frontend/[id]/page.tsx
+++ src/app/projects/frontend/[id]/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { FrontendProjects } from "../../../../db/FrontendProjects";
 
-export default function Project() {
+type ProjectProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default function Project({ params }: ProjectProps) {
+  const project = FrontendProjects.find((item) => item.id === params.id);
+
   return (
-    <div>Project</div>
-  )
+    <div className="text-white">
+      <h1 className="font-[700] text-4xl">{project?.title}</h1>
+    </div>
+  );
 }
-
-
